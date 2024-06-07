c60e2559fa378f5b6630830cda779a6b85648701
Jose A. Cuba <jcubamedellin@gmail.com>
2024-06-06 20:36:43 -0600

    Inital project

---
+++ productos-backend/services-products/src/main/java/com/prueba/productos/services_products/ServicesProductsApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.productos.services_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesProductsApplication.class, args);
+	}
+
+}
